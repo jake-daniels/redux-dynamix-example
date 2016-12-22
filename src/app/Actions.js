e60f2ed441e1215503bc0047ef9c42405636c10a
jake-daniels <soltes.jakub@gmail.com>
2016-12-22 16:29:13 +0100

    Added dynamic injection of reducers for Red and Purple modules.

--- src/app/Actions.js
+++ src/app/Actions.js
@@ -1,4 +1,8 @@
 
+import {injectReducer, ejectReducer} from 'redux-dynamix/index'
+import RedReducer from 'modules/red/Reducer'
+import PurpleReducer from 'modules/purple/Reducer'
+
 export const Types = {
 	INJECT_RED_MODULE:  'INJECT_RED_MODULE',
 	EJECT_RED_MODULE:  'EJECT_RED_MODULE',
@@ -6,9 +10,24 @@ export const Types = {
 	EJECT_PURPLE_MODULE:  'EJECT_PURPLE_MODULE',
 }
 
+const RED_MODULE_KEY = 'red'
+const PURPLE_MODULE_KEY = 'purple'
+
 export const Actions = {
-	injectRedModule: () => ({type: Types.INJECT_RED_MODULE}),
-	ejectRedModule: () => ({type: Types.EJECT_RED_MODULE}),
-	injectPurpleModule: () => ({type: Types.INJECT_PURPLE_MODULE}),
-	ejectPurpleModule: () => ({type: Types.EJECT_PURPLE_MODULE}),
+	injectRedModule: () => {
+		injectReducer(RED_MODULE_KEY, RedReducer)
+		return {type: Types.INJECT_RED_MODULE}
+	},
+	ejectRedModule: () => {
+		ejectReducer(RED_MODULE_KEY)
+		return {type: Types.EJECT_RED_MODULE}
+	},
+	injectPurpleModule: () => {
+		injectReducer(PURPLE_MODULE_KEY, PurpleReducer)
+		return {type: Types.INJECT_PURPLE_MODULE}
+	},
+	ejectPurpleModule: () => {
+		ejectReducer(PURPLE_MODULE_KEY)
+		return {type: Types.EJECT_PURPLE_MODULE}
+	},
 }
--- src/app/RootReducer.js
+++ src/app/RootReducer.js
@@ -3,12 +3,8 @@ import { combineReducers } from 'redux'
 
 import AppReducer from 'app/Reducer'
 import BlueReducer from 'modules/blue/Reducer'
-import RedReducer from 'modules/red/Reducer'
-import PurpleReducer from 'modules/purple/Reducer'
 
 export default combineReducers({
 	app: AppReducer,
 	blue: BlueReducer,
-	red: RedReducer,
-	purple: PurpleReducer,
 })
